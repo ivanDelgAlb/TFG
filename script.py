519646d7ecc143959470b5348b3ae283348efdcd
marinasayago <marsay2002@uma.es>
2024-02-09 11:14:13 +0100

    Retoques

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import schedule
 import time
 
 
-def getInformation():
+def obtener_informacion():
         # Cargar tu cuenta de IBM Quantum Experience
     # Asegúrate de reemplazar 'tu_token' con tu token real
     provider = IBMProvider(token='4fdcd337fa47374d321d5f6d9fcd74c771cdc846e23e51f7d1256ce4839a44dfbc543b009a3fd6111ce286e9ec69b15e943a4a2f5b15291c98394127a22c82df')
@@ -18,16 +18,16 @@ def getInformation():
     for backend in backends:
         try:
             # Obtener información actualizada del backend
-            backendObtained = provider.get_backend(name=backend.name)
+            backend_obtenido = provider.get_backend(name=backend.name)
             
-            partes = backendObtained.backend_version.split('.')
+            partes = backend_obtenido.backend_version.split('.')
             primer_numero = int(partes[0])
 
             # Verificar si el primer número es 1 o 2
             if primer_numero in [1, 2]:
                 with open(nombre_archivo, 'w') as archivo:
                     try:
-                        properties = backendObtained.properties()
+                        properties = backend_obtenido.properties()
                         #print("Escribiendo:", properties.to_dict())
                         archivo.write(str(properties.to_dict()))
                     except TypeError as e:
@@ -36,12 +36,12 @@ def getInformation():
             # Verificar si el primer número es 2
             if primer_numero == 2:
                 with open(nombre_archivo, 'a') as archivo:  # Usar 'a' para agregar al archivo existente
-                    configuration = backendObtained.configuration()
+                    configuration = backend_obtenido.configuration()
                     archivo.write(str(configuration.to_dict()))
 
 
             #with open(nombre_archivo, 'w') as archivo:  # Usar 'a' para agregar al archivo existente
-            #      qubitProperties = backendObtained.qubit_properties(qubit)
+            #      qubitProperties = backend_obtenido.qubit_properties(qubit)
             #     archivo.write(str(qubitProperties.to_dict()))
 
         except Exception as e:
@@ -52,10 +52,10 @@ def getInformation():
 print("Programa iniciado.")
 
 # Ejecutar inmediatamente al inicio
-getInformation()
+obtener_informacion()
 
 # Programar la ejecución cada dos horas
-schedule.every(2).hours.do(getInformation)
+schedule.every(2).hours.do(obtener_informacion)
 
 # Bucle principal
 while True:

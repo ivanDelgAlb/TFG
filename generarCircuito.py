86c37ff53611f132d45391aa86cbb283ae6582e0
ivandelgadoalba@uma.es <ivandelgadoalba@uma.es>
2024-03-12 18:45:04 +0100

    Generación aleatoria de circuito

---
+++ generarCircuito.py
@@ -0,0 +1,56 @@
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import HGate, DCXGate
+from qiskit.converters import circuit_to_dag
+from qiskit.visualization import dag_drawer
+import random
+
+def generar_circuito(n_qubits, puertas_1_qubit, puertas_2_qubit):
+
+    total_puertas = puertas_1_qubit + puertas_2_qubit
+    circuito = QuantumCircuit(n_qubits)
+
+    array_qubits_1 = []
+    array_qubits_2 = []
+
+    for i in range(n_qubits):
+        array_qubits_1.append(i)
+        array_qubits_2.append(i)
+
+    while total_puertas > 0:
+        if random.randint(1, total_puertas) <= puertas_1_qubit:
+
+            qubit_elegido = random.choice(array_qubits_1)
+            array_qubits_1.remove(qubit_elegido)
+
+            circuito.append(HGate(), [qubit_elegido])
+            
+            puertas_1_qubit -= 1
+
+            if not array_qubits_1:
+                for i in range(n_qubits):
+                    array_qubits_1.append(i)
+            
+        else:
+
+            qubit_elegido_1 = random.choice(array_qubits_2)
+            array_qubits_2.remove(qubit_elegido_1)
+            qubit_elegido_2 = random.choice(array_qubits_2)
+            array_qubits_2.remove(qubit_elegido_2)
+
+            circuito.append(DCXGate(), [qubit_elegido_1, qubit_elegido_2])
+            
+            if not array_qubits_2:
+                for i in range(n_qubits):
+                    array_qubits_2.append(i)
+
+        total_puertas -= 1
+
+    return circuito
+
+circuito = generar_circuito(4, 3, 5)
+circuito.draw(output='mpl', filename='circuit.png')
+
+'''
+    dag = circuit_to_dag(circuito)
+    print(dag.depth())
+'''

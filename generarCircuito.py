7577ec518197a38598a709e28fbd4808e93f32c5
ivandelgadoalba@uma.es <ivandelgadoalba@uma.es>
2024-03-15 21:28:45 +0100

    Añadidas puertas a la generación de circuito

--- generarCircuito.py
+++ generarCircuito.py
@@ -1,8 +1,9 @@
 from qiskit import QuantumCircuit
-from qiskit.circuit.library import HGate, DCXGate
+from qiskit.circuit.library import HGate, DCXGate, TGate, PhaseGate
 from qiskit.converters import circuit_to_dag
 from qiskit.visualization import dag_drawer
 import random
+import math
 
 def generar_circuito(n_qubits, puertas_1_qubit, puertas_2_qubit):
 
@@ -22,7 +23,18 @@ def generar_circuito(n_qubits, puertas_1_qubit, puertas_2_qubit):
             qubit_elegido = random.choice(array_qubits_1)
             array_qubits_1.remove(qubit_elegido)
 
-            circuito.append(HGate(), [qubit_elegido])
+            puerta = random.randint(1, 3)
+
+            if puerta == 1:
+                # Puerta de T
+                circuito.append(TGate(), [qubit_elegido])
+            elif puerta == 2:
+                # Puerta de fase
+                theta = random.random() * 2 * math.pi
+                circuito.append(PhaseGate(theta=theta), [qubit_elegido])
+            else: 
+                # Puerta Hadamard
+                circuito.append(HGate(), [qubit_elegido])
             
             puertas_1_qubit -= 1
 
@@ -47,10 +59,11 @@ def generar_circuito(n_qubits, puertas_1_qubit, puertas_2_qubit):
 
     return circuito
 
+'''
 circuito = generar_circuito(4, 3, 5)
 circuito.draw(output='mpl', filename='circuit.png')
 
-'''
+
     dag = circuit_to_dag(circuito)
     print(dag.depth())
 '''

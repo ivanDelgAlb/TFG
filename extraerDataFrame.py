e187b123e3019fff952606b4dd24ef3ede1530ab
ivandelgadoalba@uma.es <ivandelgadoalba@uma.es>
2024-04-01 18:46:40 +0200

    Update extraerDataFrame.py

--- extraerDataFrame.py
+++ extraerDataFrame.py
@@ -11,11 +11,11 @@ db = client['TFG']
 datos = db[collection_name_Origen].find({'name': 'ibm_brisbane'})
 
 dataFrame = [
-    ['ds', 'y', 'T2', 'probMeas0Prep1', 'probMeas1Prep0']
+    ['ds', 'y', 'T2', 'probMeas0Prep1', 'probMeas1Prep0', 'readout_error']
 ]
 
 for item in datos[0]['data']:
-    dataFrame.append([item['date'], item['T1']['media'], item['T2']['media'], item['prob_meas0_prep1']['media'], item['prob_meas1_prep0']['media']
+    dataFrame.append([item['date'], item['T1']['media'], item['T2']['media'], item['prob_meas0_prep1']['media'], item['prob_meas1_prep0']['media'], item['readout_error']['media']
                     ])
 
 nombre_archivo = 'datos.csv'

9ec79da02409a1e4eddf8514c3d05140519d4489
ivandelgadoalba@uma.es <ivandelgadoalba@uma.es>
2024-04-06 11:28:19 +0200

    Creación de todos los dataframe

--- extraerDataFrame.py
+++ extraerDataFrame.py
@@ -10,19 +10,74 @@ db = client['TFG']
 
 datos = db[collection_name_Origen].find({'name': 'ibm_brisbane'})
 
-dataFrame = [
+dataFrame_T1 = [
     ['ds', 'y', 'T2', 'probMeas0Prep1', 'probMeas1Prep0', 'readout_error']
 ]
 
-for item in datos[0]['data']:
-    dataFrame.append([item['date'], item['T1']['media'], item['T2']['media'], item['prob_meas0_prep1']['media'], item['prob_meas1_prep0']['media'], item['readout_error']['media']
-                    ])
+dataFrame_T2 = [
+    ['ds', 'y', 'T1', 'probMeas0Prep1', 'probMeas1Prep0', 'readout_error']
+]
 
-nombre_archivo = 'datos.csv'
+dataFrame_Prob0 = [
+    ['ds', 'y', 'T2', 'T1', 'probMeas1Prep0', 'readout_error']
+]
+
+dataFrame_Prob1 = [
+    ['ds', 'y', 'T2', 'probMeas0Prep1', 'T1', 'readout_error']
+]
+
+dataFrame_error = [
+    ['ds', 'y', 'T2', 'probMeas0Prep1', 'probMeas1Prep0', 'T1']
+]
+
+for item in datos[0]['data']:
+    dataFrame_T1.append([item['date'], item['T1']['media'], item['T2']['media'], item['prob_meas0_prep1']['media'], item['prob_meas1_prep0']['media'], item['readout_error']['media']])
+    dataFrame_T2.append([item['date'], item['T2']['media'], item['T1']['media'], item['prob_meas0_prep1']['media'], item['prob_meas1_prep0']['media'], item['readout_error']['media']])
+    dataFrame_Prob0.append([item['date'], item['prob_meas0_prep1']['media'], item['T2']['media'], item['T1']['media'], item['prob_meas1_prep0']['media'], item['readout_error']['media']])
+    dataFrame_Prob1.append([item['date'], item['prob_meas1_prep0']['media'], item['T2']['media'], item['prob_meas0_prep1']['media'], item['T1']['media'], item['readout_error']['media']])
+    dataFrame_error.append([item['date'], item['readout_error']['media'], item['T2']['media'], item['prob_meas0_prep1']['media'], item['prob_meas1_prep0']['media'], item['T1']['media']])
+
+nombre_archivo = 'dataframeT1.csv'
 
 with open(nombre_archivo, 'w', newline='') as archivo_csv:
     
     escritor_csv = csv.writer(archivo_csv)
     
-    for fila in dataFrame:
+    for fila in dataFrame_T1:
+        escritor_csv.writerow(fila)
+
+nombre_archivo = 'dataframeT2.csv'
+
+with open(nombre_archivo, 'w', newline='') as archivo_csv:
+    
+    escritor_csv = csv.writer(archivo_csv)
+    
+    for fila in dataFrame_T2:
+        escritor_csv.writerow(fila)
+
+nombre_archivo = 'dataframeProb0.csv'
+
+with open(nombre_archivo, 'w', newline='') as archivo_csv:
+    
+    escritor_csv = csv.writer(archivo_csv)
+    
+    for fila in dataFrame_Prob0:
+        escritor_csv.writerow(fila)
+
+nombre_archivo = 'dataframeProb1.csv'
+
+with open(nombre_archivo, 'w', newline='') as archivo_csv:
+    
+    escritor_csv = csv.writer(archivo_csv)
+    
+    for fila in dataFrame_Prob1:
+        escritor_csv.writerow(fila)
+    
+nombre_archivo = 'dataframeError.csv'
+
+with open(nombre_archivo, 'w', newline='') as archivo_csv:
+    
+    escritor_csv = csv.writer(archivo_csv)
+    
+    for fila in dataFrame_error:
         escritor_csv.writerow(fila)

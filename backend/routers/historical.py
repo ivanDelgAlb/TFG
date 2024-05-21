c208e66c37498e6b88a54b227c577de8ffbcae2f
marinasayago <marsay2002@uma.es>
2024-05-21 17:59:56 +0200

    Historical

--- backend/routers/historical.py
+++ backend/routers/historical.py
@@ -1,44 +1,52 @@
 from typing import List, Dict, Union
 from fastapi import APIRouter
 import pandas as pd
+from pydantic import BaseModel
 
 router = APIRouter()
 
-@router.get("/")
-async def historical():
+# Clase para definir la estructura de los datos enviados desde el frontend
+class PredictionData(BaseModel):
+    machine: str
+
+@router.post("/")
+async def historical(data: PredictionData):
+    print(data.machine)
     historical = []
 
-    qubits = qubitsCalibration()
-    gates = gatesCalibration()
+    qubits = qubitsCalibration(data.machine)
+    gates = gatesCalibration(data.machine)
     historical.append(qubits)
     historical.append(gates)
         
     return {"historical": historical}
 
-def qubitsCalibration():
+def qubitsCalibration(machine):
     dataframes_directory = 'backend/dataframes_neuralProphet/'
-    machines = ['Brisbane', 'Kyoto', 'Osaka']
+    machine = machine.split(" ")[1].capitalize()
 
     # Leer el archivo CSV
-    qubits = pd.read_csv(dataframes_directory + 'dataframeT1' + machines[0] + '.csv', encoding="latin1")
+    qubits = pd.read_csv(dataframes_directory + 'dataframeT1' + machine + '.csv', encoding="latin1")
 
     # Cambiar el nombre de la columna
     qubits = qubits.rename(columns={'y': 'T1'})
 
-    # Eliminar una columna
+    # Cambiar el nombre de la columna
     qubits = qubits.rename(columns={'ds': 'Date'})
     
     return {'qubits': qubits.to_dict(orient='records')} # Convertir DataFrame a lista de diccionarios
 
 
-def gatesCalibration() -> Dict[str, Union[str, str]]:
+def gatesCalibration(machine) -> Dict[str, Union[str, str]]:
     dataframes_directory = 'backend/dataframes_gates/'
-    machines = ['Brisbane', 'Kyoto', 'Osaka']
+    machine = machine.split(" ")[1].capitalize()
 
     # Leer el archivo CSV
-    gates = pd.read_csv(dataframes_directory + 'dataframe_Gates' + machines[0] + '.csv', encoding="latin1")
+    gates = pd.read_csv(dataframes_directory + 'dataframe_Gates' + machine + '.csv', encoding="latin1")
 
-    # Eliminar una columna
-    gates = gates.drop(columns=['date'])
+    # Cambiar el nombre de la columna
+    gates = gates.rename(columns={'date': 'Date'})
+
+    print(gates)
     
     return {'gates': gates.to_dict(orient='records')}  # Convertir DataFrame a lista de diccionarios
--- frontend/src/Historical/Historical.js
+++ frontend/src/Historical/Historical.js
@@ -1,128 +1,133 @@
 import React, { useState, useEffect } from "react";
 import Graph from '../Graph/Graph'; // Importa el componente de la gráfica
+import './Historical.css'; // Asegúrate de crear este archivo CSS para los estilos
 
 function Historical() {
-    const [showCalibrationGraphsQubits, setShowCalibrationGraphsQubits] = useState(false);
-    const [showCalibrationGraphsGates, setShowCalibrationGraphsGates] = useState(false);
     const [calibration, setCalibration] = useState([]);
-    const [machine, setMachine] = useState("");
+    const [selectedMachine, setSelectedMachine] = useState("");
     const [option, setOption] = useState("");
     const [error, setError] = useState(null);
+    const [showCalibrationGraphs, setShowCalibrationGraphs] = useState(false);
+
+    const machines = ["ibm Brisbane", "ibm Kyoto", "ibm Osaka"];
 
     const handleButtonCalibration = () => {
-        setError(null)
-        if(!machine){
-            setError("You must select a machine")
+        setError(null);
+        if (!selectedMachine) {
+            setError("You must select a machine");
             return;
-        }else if(!option){
-            setError("You must select an option")
+        } else if (!option) {
+            setError("You must select an option");
             return;
-        }
-        if(option === "Qubits"){
-            setShowCalibrationGraphsQubits(!showCalibrationGraphsQubits);
-            setShowCalibrationGraphsGates(false);
+        }else if(showCalibrationGraphs){
+            setShowCalibrationGraphs(false);
         }else{
-            setShowCalibrationGraphsGates(!showCalibrationGraphsGates);
-            setShowCalibrationGraphsQubits(false);
+            setShowCalibrationGraphs(true);
         }
-    };
-
-    const handleChangeMachine = (event) => {
-        setMachine(event.target.value);
     };
 
     const handleChangeOption = (event) => {
         setOption(event.target.value);
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:8000/historical', {
-                    method: 'GET',
-                    headers: {
-                      'Content-Type': 'application/json'
-                    }
+    const handleTabChange = async (machine) => {
+        console.log(machine)
+        setSelectedMachine(machine);
+        setError(null);
+
+        try {
+            const response = await fetch('http://localhost:8000/historical', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    machine: machine
                   })
-                const data = await response.json();
-                
-                setCalibration(data.historical);
-                console.log(data.historical)
+              })
+            const data = await response.json();
+            setCalibration(data.historical);
+            console.log(data.historical);
 
-            } catch (error) {
-                console.error("Error fetching calibration data:", error);
-            }
-        };
-
-        fetchData();
-    }, []); // El segundo argumento del useEffect indica que solo se ejecutará una vez, al cargar el componente
+        } catch (error) {
+            console.error("Error fetching calibration data:", error);
+        }
+    };
 
     return (
         <div className="container">
             <div className="bar">
                 <h1 className="title">Historical</h1>
             </div>
-            <div className="selectors-row">
-                <div className="machine-selector">
-                    <select value={machine} onChange={handleChangeMachine} className="selector-option-select">
-                        <option value="">Choose a machine</option>
-                        <option value="ibm Brisbane">ibm Brisbane</option>
-                        <option value="ibm Kyoto">ibm Kyoto</option>
-                        <option value="ibm Osaka">ibm Osaka</option>
-                    </select>
-                </div>
-                <div className="option-selector">
-                    <select value={option} onChange={handleChangeOption} className="selector-option-select">
-                        <option value="">Choose an option</option>
-                        <option value="Qubits">Qubits</option>
-                        <option value="Gates">Gates</option>
-                    </select>
-                </div>
+
+            <div className="tabs">
+                {machines.map((machine) => (
+                    <button 
+                        key={machine}
+                        className={`tab ${selectedMachine === machine ? "active" : ""}`} 
+                        onClick={() => handleTabChange(machine)}
+                    >
+                        {machine}
+                    </button>
+                ))}
             </div>
 
-            {error && <p className="error-message">{error}</p>}
-
-            <div className="container-button">
-                <button onClick={handleButtonCalibration} className="button">
-                    {showCalibrationGraphsQubits || showCalibrationGraphsGates ? "Hide Calibration Charts" : "Show Calibration Charts"}
-                </button>
-            </div>
-
-            {/* Aquí puedes renderizar tus gráficos de calibración si showCalibrationGraphsQubits es true */}
-            {showCalibrationGraphsQubits && calibration && (
+            {selectedMachine && (
                 <div>
-                    <div style={{ marginBottom: '40px' }}>
-                        <h2>Historical T1:</h2>
-                        <Graph predictions={calibration[0].qubits} type={'T1'} historical={true} />
+                    <div className="option-selector">
+                        <select value={option} onChange={handleChangeOption} className="selector-option-select">
+                            <option value="">Choose an option</option>
+                            <option value="Qubits">Qubits</option>
+                            <option value="Gates">Gates</option>
+                            <option value="Error">Error</option>
+                        </select>
                     </div>
-                    <div style={{ marginBottom: '40px' }}>
-                        <h2>Historical T2:</h2>
-                        <Graph predictions={calibration[0].qubits} type={'T2'} historical={true} />
+
+                    {error && <p className="error-message">{error}</p>}
+
+                    <div className="container-button">
+                        <button onClick={handleButtonCalibration} className="button">
+                            {showCalibrationGraphs ? "Hide Calibration Charts" : "Show Calibration Charts"}
+                        </button>
                     </div>
-                    <div style={{ marginBottom: '40px' }}>
-                        <h2>Historical Prob0:</h2>
-                        <Graph predictions={calibration[0].qubits} type={'probMeas0Prep1'} historical={true} />
-                    </div>
-                    <div style={{ marginBottom: '40px' }}>
-                        <h2>Historical Prob1:</h2>
-                        <Graph predictions={calibration[0].qubits} type={'probMeas1Prep0'} historical={true} />
-                    </div>
-                    <div style={{ marginBottom: '40px' }}>
-                        <h2>Historical readout_error:</h2>
-                        <Graph predictions={calibration[0].qubits} type={'readout_error'} historical={true} />
-                    </div>
-                </div>
-            )}
-            {showCalibrationGraphsGates && calibration && (
-                <div>
-                    <div style={{ marginBottom: '40px' }}>
-                        <h2>Historical Gate error of one-qubit input:</h2>
-                        <Graph predictions={calibration[1].gates} type={'gate_error_one_qubit'} historical={true} />
-                    </div>
-                    <div style={{ marginBottom: '40px' }}>
-                        <h2>Historical Gate error of two-qubit input:</h2>
-                        <Graph predictions={calibration[1].gates} type={'gate_error_two_qubit'} historical={true} />
-                    </div>
+
+                    {option === "Qubits" && calibration && showCalibrationGraphs && (
+                        <div>
+                            <div style={{ marginBottom: '40px' }}>
+                                <h2>Historical T1:</h2>
+                                <Graph predictions={calibration[0].qubits} type={'T1'} historical={true} />
+                            </div>
+                            <div style={{ marginBottom: '40px' }}>
+                                <h2>Historical T2:</h2>
+                                <Graph predictions={calibration[0].qubits} type={'T2'} historical={true} />
+                            </div>
+                            <div style={{ marginBottom: '40px' }}>
+                                <h2>Historical Prob0:</h2>
+                                <Graph predictions={calibration[0].qubits} type={'probMeas0Prep1'} historical={true} />
+                            </div>
+                            <div style={{ marginBottom: '40px' }}>
+                                <h2>Historical Prob1:</h2>
+                                <Graph predictions={calibration[0].qubits} type={'probMeas1Prep0'} historical={true} />
+                            </div>
+                            <div style={{ marginBottom: '40px' }}>
+                                <h2>Historical readout_error:</h2>
+                                <Graph predictions={calibration[0].qubits} type={'readout_error'} historical={true} />
+                            </div>
+                        </div>
+                    )}
+
+                    {option === "Gates" && calibration && showCalibrationGraphs &&(
+                        <div>
+                            <div style={{ marginBottom: '40px' }}>
+                                <h2>Historical Gate error of one-qubit input:</h2>
+                                <Graph predictions={calibration[1].gates} type={'gate_error_1'} historical={true} />
+                            </div>
+                            <div style={{ marginBottom: '40px' }}>
+                                <h2>Historical Gate error of two-qubit input:</h2>
+                                <Graph predictions={calibration[1].gates} type={'gate_error_2'} historical={true} />
+                            </div>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
@@ -130,3 +135,5 @@ function Historical() {
 }
 
 export default Historical;
+
+
